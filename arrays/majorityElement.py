923a55b93331a5e277c83aab8f531aa00f6d5805
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-11 15:37:40 +0530

    Add Boyer-Moore Voting Solution for majority element

--- arrays/majorityElement.py
+++ arrays/majorityElement.py
@@ -9,3 +9,18 @@ def majorityElement(nums: list[int]) -> int:
 if __name__ == "__main__":
     nums = [5,5,5,1,1,1,5]   
     print(majorityElement(nums))
+
+
+# Boyer-Moore Voting Solution
+def majorityElement(nums: list[int]) -> int:
+    res = count = 0
+
+    for num in nums:
+        if count == 0:
+            res = num
+        count += (1 if num == res else -1)
+    return res
+
+if __name__ == "__main__":
+    nums = [5,5,5,1,1,1,5]
+    print(majorityElement(nums))
