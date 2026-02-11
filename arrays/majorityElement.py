f01cb219f5a83d06a94067b9e6b34097b8e44f27
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-11 16:03:50 +0530

    Add hashmap solution for majority element problem

--- arrays/majorityElement.py
+++ arrays/majorityElement.py
@@ -24,3 +24,20 @@ def majorityElement(nums: list[int]) -> int:
 if __name__ == "__main__":
     nums = [5,5,5,1,1,1,5]
     print(majorityElement(nums))
+
+
+# Hashmap Solution
+def majorityElement(nums: list[int]) -> int:
+    count = {}
+    res = maxCount = 0
+
+    for num in nums:
+        count[num] = 1 + count.get(num, 0)
+        if maxCount < count[num]:
+            res = num
+            maxCount = count[num]
+    return res
+
+if __name__ == "__main__":
+    nums = [5,5,5,1,1,1,5]
+    print(majorityElement(nums))
