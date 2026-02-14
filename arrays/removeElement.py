72da829e112a7934acbd51d3b6fd93e3a844f61b
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-14 12:16:46 +0530

    Add problem statement for better clarity
    Added problem statement and clarified the purpose of the removeElement function.

--- arrays/removeElement.py
+++ arrays/removeElement.py
@@ -1,7 +1,16 @@
+# Problem Statement
+# You are given an integer array nums and an integer val. Your task is to remove all occurrences of val from nums in-place.
+# After removing all occurrences of val, return the number of remaining elements, say k, such that the first k elements of nums do not contain val.
+# Note:
+#     - The order of the elements which are not equal to val does not matter.
+#     - It is not necessary to consider elements beyond the first k positions of the array.
+#     - To be accepted, the first k elements of nums must contain only elements not equal to val.
+# Return k as the final result.
+
 # Brute Force Solution
 def removeElement(nums: list[int], val: int) -> int:
     ans = []
-
+    
     for num in nums:
         if num == val:
             continue
