a472a665b5274027f10c9fe684daaf9e47ca5d6e
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-14 12:13:51 +0530

    Added Problem Statement for better clarity

--- arrays/longestCommonPrefix.py
+++ arrays/longestCommonPrefix.py
@@ -1,3 +1,7 @@
+# Problem Statement
+# You are given an array of strings strs. Return the longest common prefix of all the strings.
+# If there is no longest common prefix, return an empty string "".
+
 def longestCommonPrefix(strs: list[str]) -> str:
     # Start with the first string
     prefix = strs[0]
@@ -19,3 +23,4 @@ def longestCommonPrefix(strs: list[str]) -> str:
 
     # Return the longest set of prefix
     return prefix
+
