40b557f3782682a31860684665bf9a09c8f5552d
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-14 12:09:46 +0530

    Added Problem Statement for better clarity

--- arrays/anagram.py
+++ arrays/anagram.py
@@ -1,8 +1,11 @@
+# Problem Statement:
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
 #Sorting solution
 def isAnagram(s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
-    
     return sorted(s) == sorted(t)
 
 if __name__ == "__main__":
@@ -20,7 +23,6 @@ def isAnagram(s: str, t: str) -> bool:
     for i in range(len(s)):
         countS[s[i]] = 1 + countS.get(s[i], 0)
         countT[t[i]] = 1 + countT.get(t[i], 0)
-    
     return countS == countT
 
 if __name__ == "__main__":
