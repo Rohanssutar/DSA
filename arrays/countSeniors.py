c0524d419909287e58b4030de7402dc8d14772bf
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-14 12:20:29 +0530

    Added Problem Statement for better clarity

--- arrays/countSeniors.py
+++ arrays/countSeniors.py
@@ -1,3 +1,12 @@
+# Problem Statement
+# You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15.
+# The system is such that:
+#     The first ten characters consist of the phone number of passengers.
+#     The next character denotes the gender of the person.
+#     The following two characters are used to indicate the age of the person.
+#     The last two characters determine the seat allotted to that person.
+# Return the number of passengers who are strictly more than 60 years old.
+
 # String Parsing Solution
 def count_Seniors(details: list[str]) -> int:
     count = 0
@@ -8,4 +17,5 @@ def count_Seniors(details: list[str]) -> int:
 
 if __name__ == "__main__":
     details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+
     print(count_Seniors(details))
