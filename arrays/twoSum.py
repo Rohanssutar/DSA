77a266d4a9fb7efe5cb443dce8950b22391f6154
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-14 12:07:37 +0530

    Added Problem Statement for better clarity

--- arrays/twoSum.py
+++ arrays/twoSum.py
@@ -1,3 +1,8 @@
+# Problem Statement: 
+# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. 
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+# Return the answer with the smaller index first.
+
 # Brute force Solution
 def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
@@ -14,7 +19,6 @@ if __name__ == "__main__":
 # Hashmap (One pass) Solution
 def twoSum(nums: list[int], target: int) -> list[int]:
     hashmap = {}
-
     for i, n in enumerate(nums):
         diff = target - n
         if diff in hashmap:
