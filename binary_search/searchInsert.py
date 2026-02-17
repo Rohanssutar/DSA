842925ecd247636b352a7d0922aa3d0003a5753d
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-17 19:47:01 +0530

    Added Problem Statement for better clarity

--- binary_search/searchInsert.py
+++ binary_search/searchInsert.py
@@ -1,3 +1,9 @@
+# # Problem Statement
+# You are given a sorted array of distinct integers and a target value, return the index if the target is found. 
+# If not, return the index where it would be if it were inserted in order.
+# You must write an algorithm with O(log n) runtime complexity.
+
+
 # Brute Force Solution (Binary Search)
 def search_Insert(nums: list[int], target: int) -> int:
     if target not in nums:
