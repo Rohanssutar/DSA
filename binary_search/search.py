e07445fa004c33f145b342ea7542f083d1a698f1
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-17 19:48:22 +0530

    Added Problem Statement for better clarity

--- binary_search/search.py
+++ binary_search/search.py
@@ -1,3 +1,8 @@
+# Problem Statement
+# You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+# Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+# Your solution must run in O(logn) time.
+
 # Iterative Search Solution
 def search(nums: list[int], target: int) -> int:
     left = 0
