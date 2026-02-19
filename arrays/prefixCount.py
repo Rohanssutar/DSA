07794812229dcbdd0bc4198b73e1b6977a73a6b6
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-19 18:11:51 +0530

    Added Problem Statement for better clarity

--- arrays/prefixCount.py
+++ arrays/prefixCount.py
@@ -1,3 +1,8 @@
+# Problem Statement
+# You are given an array of strings words and a string pref.
+# Return the number of strings in words that contain pref as a prefix.
+# A prefix of a string s is any leading contiguous substring of s.
+
 # Brute Force Solution
 def prefix_Count(words: list[str], pref: str) -> int:
     count = 0
@@ -24,4 +29,5 @@ def prefixCount(self, words: List[str], pref: str) -> int:
 if __name__ == "__main__":
     words = ["neetcode","neet","nee","code"]
     pref = "ne"
+
     print(prefix_Count(words, pref))
