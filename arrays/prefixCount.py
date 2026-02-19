063ff7a220719d71dd2c4a9356385a23a2a9a884
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-19 18:10:52 +0530

    Solved 1 array problem

---
+++ arrays/prefixCount.py
@@ -0,0 +1,27 @@
+# Brute Force Solution
+def prefix_Count(words: list[str], pref: str) -> int:
+    count = 0
+
+    for i in range(len(words)):
+        if pref in words[i][:len(pref)]:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    words = ["neetcode","neet","nee","code"]
+    pref = "ne"
+    print(prefix_Count(words, pref))
+
+# Built-In Method Solution
+def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+
+        for i in range(len(words)):
+            if words[i].startswith(pref):
+                count += 1
+        return count
+
+if __name__ == "__main__":
+    words = ["neetcode","neet","nee","code"]
+    pref = "ne"
+    print(prefix_Count(words, pref))
