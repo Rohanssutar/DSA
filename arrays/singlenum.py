2b524aa3732d09e8297f2dfafca21932931816a8
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-20 22:16:41 +0530

    Added Problem Statement for better clarity

--- arrays/singlenum.py
+++ arrays/singlenum.py
@@ -1,3 +1,8 @@
+# Problem Statement
+# You are given a non-empty array of integers nums. Every integer appears twice except for one.
+# Return the integer that appears only once.
+# You must implement a solution with O(n) runtime complexity and use only O(1) extra space.
+
 # Hashset Solution
 def single_Number(nums: list[int]) -> int:
         seen = set()
@@ -11,4 +16,5 @@ def single_Number(nums: list[int]) -> int:
 if __name__ == "__main__":
      nums = [3,2,3]
      print(single_Number(nums))
+
         
