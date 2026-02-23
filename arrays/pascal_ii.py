1ae041289be9d187f9a1b58dc744b819e1f3b6de
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-23 21:51:52 +0530

    Solved 1 array problem

---
+++ arrays/pascal_ii.py
@@ -0,0 +1,15 @@
+def pascal_ii(row_index: int) -> list[int]:
+    res = [[1]]
+
+    for i in range(row_index):
+        temp = [0] + res[-1] + [0]
+        rows = []
+        for j in range(len(res[-1]) +  1):
+            rows.append(temp[j] + temp[j + 1])
+        res.append(rows)
+
+    return res[row_index]
+
+if __name__ == "__main__":
+    n = 3
+    print(pascal_ii(n))
