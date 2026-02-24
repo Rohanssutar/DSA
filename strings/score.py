ff21077417a118bd8bc488d66efed30e1ca66430
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-24 21:18:20 +0530

    Added Problem Statement for better clarity

--- strings/score.py
+++ strings/score.py
@@ -1,3 +1,7 @@
+# Problem Statement
+# You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+# Return the score of s.
+
 # Iteration Solution
 def score(s: str) -> int:
     sum = 0
