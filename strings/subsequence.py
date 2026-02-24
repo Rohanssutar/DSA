d9729f8d127ed748cf34384df244a4c1f46e35bb
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-24 21:19:34 +0530

    Added Problem Statement for better clarity

--- strings/subsequence.py
+++ strings/subsequence.py
@@ -1,3 +1,8 @@
+# Problem Statement
+# You are given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters.
+# (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
 # Two-pointer solution
 def is_Subsequence(s: str, t: str) -> bool:
     i, j = 0, 0
