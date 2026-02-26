b413824079ead7a57035239cc2e5410419e85bf2
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-02-26 22:19:06 +0530

    Solved 1 stack problem

---
+++ stack/isValid.py
@@ -0,0 +1,18 @@
+def is_valid(s: str) -> bool:
+    stack = []
+    hashmap = { ")" : "(", "]" : "[", "}" : "{" }
+
+    for c in s:
+        if c in hashmap:
+            if stack and stack[-1] == hashmap[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    
+    return True if not stack else False
+
+if __name__ == "__main__":
+    s = "([{}])"
+    print(is_valid(s))
