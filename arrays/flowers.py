4f0c9ea54b01387b83ac6f727ea3c8e86cb46cfe
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-03-05 20:04:28 +0530

    Solved 1 array problem

---
+++ arrays/flowers.py
@@ -0,0 +1,37 @@
+# Brute Force Solution
+class Solution:
+    def place_Flowers(self, flowerbed: list[int], n: int) -> bool:
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f) - 1):
+            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+                f[i] = 1
+                n -= 1
+        return n <= 0
+    
+if __name__ == "__main__":
+    flowerbed = [1,0,0,0,1]
+    n = 2
+    obj = Solution()
+    print(obj.place_Flowers(flowerbed, n))
+
+
+# Optimal Solution
+class Solution:
+    def place_Flowers(self, flowerbed: list[int], n: int) -> bool:
+        empty = 0 if flowerbed[0] else 1
+
+        for f in flowerbed:
+            if f:
+                n -= int((empty - 1) / 2)
+                empty = 0
+            else:
+                empty += 1
+        
+        n -= empty // 2
+        return n <= 0
+
+if __name__ == "__main__":
+    flowerbed = [1,0,0,0,1]
+    n = 2
+    obj = Solution()
+    print(obj.place_Flowers(flowerbed, n))
