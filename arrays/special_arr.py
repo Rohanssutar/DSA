a65ec1e856f08a7f83ec79627d5c5827e63274ab
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-03-06 20:25:07 +0530

    Added Problem Statement for better clarity

--- arrays/special_arr.py
+++ arrays/special_arr.py
@@ -1,3 +1,9 @@
+# Problem Statement
+# An array is considered special if the parity of every pair of adjacent elements is different. 
+# In other words, one element in each pair must be even, and the other must be odd.
+
+# You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
 # Modulo Solution
 class Solution:
     def special_Arr(self, nums: list[int]) -> bool:
@@ -10,4 +16,5 @@ if __name__ == "__main__":
     nums = [4,3,1,6]
     obj = Solution()
     print(obj.special_Arr(nums))
+
 
