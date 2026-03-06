2539ea97cdbe977ca535ecf5b588e9ff73c29c2b
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-03-06 20:24:18 +0530

    Added Problem Statement for better clarity

--- strings/binary.py
+++ strings/binary.py
@@ -1,3 +1,9 @@
+# Problem Statement
+# An array is considered special if the parity of every pair of adjacent elements is different. 
+# In other words, one element in each pair must be even, and the other must be odd.
+
+# You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
 class Solution:
     def check_ones(self, s: str) -> bool:
         seen_Zero = False
