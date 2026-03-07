26efd3cb140cd04b868d82dc507acf8fde355bc7
Rohan S Sutar <114920029+Rohanssutar@users.noreply.github.com>
2026-03-07 21:02:46 +0530

    Solved 1 string problem

---
+++ strings/balloon.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = {}
+
+        for c in text:
+            count[c] = count.get(c, 0) + 1
+
+        return min(
+            count.get('b', 0),
+            count.get('a', 0),
+            count.get('l', 0) // 2,
+            count.get('o', 0) // 2,
+            count.get('n', 0)
+        )
+    
+if __name__ == "__main__":
+    text = "loonbalxballpoon"
+    obj = Solution()
+    print(obj.maxNumberOfBalloons(text))
