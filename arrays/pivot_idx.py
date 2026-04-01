cf828df2e044646d23c6a3d966a41f8feac32ddc
Rohanssutar <rohan.sutar0804@gmail.com>
2026-04-01 22:18:07 +0530

    Added problem statement for better clarity

--- arrays/pivot_idx.py
+++ arrays/pivot_idx.py
@@ -1,3 +1,12 @@
+# Problem Statement
+# You are given an array of integers nums, calculate the pivot index of this array.
+
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+# Return the leftmost pivot index. If no such index exists, return -1.
+
+# Brute Force Solution
 class Solution:
     def pivot_Idx(self, nums: list[int]) -> int:
         n = len(nums)
@@ -16,6 +25,7 @@ if __name__ == "__main__":
     obj = Solution()
     print(obj.pivot_Idx(nums))
 
+# Optimal Solution
 class Solution:
     def pivot_Idx(self, nums: list[int]) -> int:
         total_sum = sum(nums)
