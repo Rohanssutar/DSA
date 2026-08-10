125dbf857da8055b97c42797a3b07443eeb1c8ca
Rohanssutar <rohan.sutar0804@gmail.com>
2026-08-10 19:01:43 +0530

    Improved Syntax

--- binary_search/searchInsert.py
+++ binary_search/searchInsert.py
@@ -28,17 +28,17 @@ if __name__ == "__main__":
 
 # Binary Search Solution
 class Solution:
-def search_Insert(nums: list[int], target: int) -> int:
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = left + ((right- left) // 2)
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return left
+    def search_Insert(nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right- left) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
 
 if __name__ == "__main__":
     obj = Solution()
