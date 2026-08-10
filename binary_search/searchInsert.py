1914af7136ce703b3abcfed083549c47e1be1155
Rohanssutar <rohan.sutar0804@gmail.com>
2026-08-10 18:58:53 +0530

    Solved 1 binary search problem

--- binary_search/searchInsert.py
+++ binary_search/searchInsert.py
@@ -30,13 +30,13 @@ if __name__ == "__main__":
 def search_Insert(nums: list[int], target: int) -> int:
     left, right = 0, len(nums) - 1
     while left <= right:
-        middle = left + ((right- left) // 2)
-        if nums[middle] == target:
-            return middle
-        elif nums[middle] > target:
-            right = middle - 1
+        mid = left + ((right- left) // 2)
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
         else:
-            left = middle + 1
+            left = mid + 1
     return left
 
 if __name__ == "__main__":
