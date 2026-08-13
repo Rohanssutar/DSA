68f156dbcd2fe597e56e482e607c192858173ee9
Rohanssutar <rohan.sutar0804@gmail.com>
2026-08-13 20:19:30 +0530

    Solved 2 Arrays problems

--- arrays/pascal.py
+++ arrays/pascal.py
@@ -2,18 +2,20 @@
 # You are given an integer numRows, return the first numRows of Pascal's triangle.
 # In Pascal's triangle, each number is the sum of the two numbers directly above
 
-def pascal(numrows: int) -> list[list[int]]:
-    res = [[1]]
+class Solution:
+    def pascal(self, numrows: int) -> list[list[int]]:
+        res = [[1]]
 
-    for i in range(numrows - 1):
-        temp = [0] + res[-1] + [0]
-        rows = []
-        for j in range(len(res[-1]) + 1):
-            rows.append(temp[j] + temp[j + 1])
-        res.append(rows)
-        
-    return res
+        for i in range(numrows - 1):
+            temp = [0] + res[-1] + [0]
+            rows = []
+            for j in range(len(res[-1]) + 1):
+                rows.append(temp[j] + temp[j + 1])
+            res.append(rows)
+            
+        return res
 
 if __name__ == "__main__":
+    obj = Solution()
     n = 5
-    print(pascal(n))
+    print(obj.pascal(n))
--- arrays/pascal_ii.py
+++ arrays/pascal_ii.py
@@ -1,15 +1,17 @@
-def pascal_ii(row_index: int) -> list[int]:
-    res = [[1]]
+class Solution:
+    def pascal_ii(self, row_index: int) -> list[int]:
+        res = [[1]]
 
-    for i in range(row_index):
-        temp = [0] + res[-1] + [0]
-        rows = []
-        for j in range(len(res[-1]) +  1):
-            rows.append(temp[j] + temp[j + 1])
-        res.append(rows)
+        for i in range(row_index):
+            temp = [0] + res[-1] + [0]
+            rows = []
+            for j in range(len(res[-1]) +  1):
+                rows.append(temp[j] + temp[j + 1])
+            res.append(rows)
 
-    return res[row_index]
+        return res[row_index]
 
 if __name__ == "__main__":
+    obj = Solution()
     n = 3
-    print(pascal_ii(n))
+    print(obj.pascal_ii(n))
